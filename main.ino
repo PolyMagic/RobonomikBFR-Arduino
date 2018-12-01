bd6992c4de8d4ea83fc696f7236524d0260b86af
PolyMagic <bartek.marynczak@op.pl>
2018-12-01 13:43:36 +0100

    init

---
+++ main.ino
@@ -0,0 +1,38 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial btSerial(2, 3); // RX, TX
+String mess;
+
+int esc1 = 5; 
+
+void setup() {
+  pinMode(esc1,OUTPUT);
+
+
+  Serial.begin(9600);
+  btSerial.begin(9600);
+}
+
+void loop() {
+
+  if(btSerial.available()){
+    mess = btSerial.readStringUntil('\n');
+  }
+
+
+  if(mess.length()>0){
+    
+
+    if(mess[0]=='P'){
+      int number = mess.substring(1).toInt();
+      Serial.println(number);
+
+      number = map(number,0,100,0,255);
+      analogWrite(esc1,number);
+    }
+
+    mess="";
+  }
+
+
+}
